e4826e367dd7492b144aaa492b4bdb00b5c139cd
ch0mper <hannahchang17@gmail.com>
2018-10-11 16:03:08 -0500

    passed tests

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+
+class Dog
+
+    @@all = []
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.all
+        @@all.collect do | dog |
+            puts dog.name
+        end
+    end
+
+end
+
+pluto = Dog.new("Pluto")
