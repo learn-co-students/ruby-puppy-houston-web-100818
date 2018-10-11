8adc7f8e9a65b394461ed48210d8297923c39e2e
Hannah Chang <hannahchang17@gmail.com>
2018-10-11 21:05:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,3 @@
-
 class Dog
 
     @@all = []
@@ -23,3 +22,29 @@ class Dog
 end
 
 pluto = Dog.new("Pluto")
+
+
+# class Dog
+
+#     @@all = []
+
+#     attr_reader :name
+
+#     def initialize(name)
+#         @name = name
+#         @@all << self
+#     end
+
+#     def self.clear_all
+#         @@all = []
+#     end
+
+#     def self.all
+#         @@all.collect do | dog |
+#             puts dog.name
+#         end
+#     end
+
+# end
+
+# pluto = Dog.new("Pluto")
